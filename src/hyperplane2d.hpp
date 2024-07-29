2f59909b48ea57a8276f24f0cf621ec15fc52217
Shiqing Wei <shiqing.wei@outlook.com>
2024-07-29 19:35:57 -0400

    improved comments for hyperplane2d and hyperplane3d

--- src/hyperplane2d.hpp
+++ src/hyperplane2d.hpp
@@ -11,15 +11,15 @@
 
 class Hyperplane2d : public ScalingFunction2d {
     public:
-        xt::xtensor<double, 1> a; // vector a in F(P) = a^T P + b, shape: (2,)
-        double b; // scalar b in F(P) = a^T P + b
+        xt::xtensor<double, 1> a; // vector a in F(P) = a^T P + b + 1, shape: (2,)
+        double b; // scalar b in F(P) = a^T P + b + 1
 
         /**
          * @brief Construct a new Hyperplane2d object
          * 
          * @param isMoving_ True if the scaling function is moving, false otherwise
-         * @param a_ Vector a in F(P) = a^T P + b, shape: (2,)
-         * @param b_ Scalar b in F(P) = a^T P + b
+         * @param a_ Vector a in F(P) = a^T P + b + 1, shape: (2,)
+         * @param b_ Scalar b in F(P) = a^T P + b + 1
          */
         Hyperplane2d(bool isMoving_, const xt::xtensor<double, 1>& a_, double b_) 
         : ScalingFunction2d(isMoving_), a(a_), b(b_) {
@@ -30,7 +30,7 @@ class Hyperplane2d : public ScalingFunction2d {
         ~Hyperplane2d() = default;
 
         /**
-         * @brief Calculate the scaling function F(P) = a^T P + b.
+         * @brief Calculate the scaling function F(P) = a^T P + b + 1.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return double F(P)
@@ -38,7 +38,7 @@ class Hyperplane2d : public ScalingFunction2d {
         double getBodyF(const xt::xtensor<double, 1>& P) const override;
 
         /**
-         * @brief Calculate the gradient of the scaling function F(P) = a^T P + b w.r.t. P.
+         * @brief Calculate the gradient of the scaling function F(P) = a^T P + b + 1 w.r.t. P.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return xt::xtensor<double, 1> dF/dP, shape: (dim_p,)
@@ -46,7 +46,7 @@ class Hyperplane2d : public ScalingFunction2d {
         xt::xtensor<double, 1> getBodyFdP(const xt::xtensor<double, 1>& P) const override;
 
         /**
-         * @brief Calculate the Hessian of the scaling function F(P) = a^T P + b w.r.t. P.
+         * @brief Calculate the Hessian of the scaling function F(P) = a^T P + b + 1 w.r.t. P.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return xt::xtensor<double, 2> d^2F/dPdP, shape: (dim_p, dim_p)
@@ -54,7 +54,7 @@ class Hyperplane2d : public ScalingFunction2d {
         xt::xtensor<double, 2> getBodyFdPdP(const xt::xtensor<double, 1>& P) const override;
 
         /**
-         * @brief Calculate the third order derivative of the scaling function F(P) = a^T P + b w.r.t. P.
+         * @brief Calculate the third order derivative of the scaling function F(P) = a^T P + b + 1 w.r.t. P.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return xt::xtensor<double, 3> d^3F/dPdPdP, shape: (dim_p, dim_p, dim_p)
--- src/hyperplane3d.hpp
+++ src/hyperplane3d.hpp
@@ -12,15 +12,15 @@
 
 class Hyperplane3d : public ScalingFunction3d {
     public:
-        xt::xtensor<double, 1> a; // vector a in F(P) = a^T P + b, shape: (3,)
-        double b; // scalar b in F(P) = a^T P + b
+        xt::xtensor<double, 1> a; // vector a in F(P) = a^T P + b + 1, shape: (3,)
+        double b; // scalar b in F(P) = a^T P + b + 1
 
         /**
          * @brief Construct a new Hyperplane3d object
          * 
          * @param isMoving_ True if the scaling function is moving, false otherwise
-         * @param a_ Vector a in F(P) = a^T P + b, shape: (3,)
-         * @param b_ Scalar b in F(P) = a^T P + b
+         * @param a_ Vector a in F(P) = a^T P + b + 1, shape: (3,)
+         * @param b_ Scalar b in F(P) = a^T P + b + 1
          */
         Hyperplane3d(bool isMoving_, const xt::xtensor<double, 1>& a_, double b_) 
         : ScalingFunction3d(isMoving_), a(a_), b(b_) {
@@ -31,7 +31,7 @@ class Hyperplane3d : public ScalingFunction3d {
         ~Hyperplane3d() = default;
 
         /**
-         * @brief Calculate the scaling function F(P) = a^T P + b.
+         * @brief Calculate the scaling function F(P) = a^T P + b + 1.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return double F(P)
@@ -39,7 +39,7 @@ class Hyperplane3d : public ScalingFunction3d {
         double getBodyF(const xt::xtensor<double, 1>& P) const override;
 
         /**
-         * @brief Calculate the gradient of the scaling function F(P) = a^T P + b w.r.t. P.
+         * @brief Calculate the gradient of the scaling function F(P) = a^T P + b + 1 w.r.t. P.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return xt::xtensor<double, 1> dF/dP, shape: (dim_p,)
@@ -47,7 +47,7 @@ class Hyperplane3d : public ScalingFunction3d {
         xt::xtensor<double, 1> getBodyFdP(const xt::xtensor<double, 1>& P) const override;
 
         /**
-         * @brief Calculate the Hessian of the scaling function F(P) = a^T P + b w.r.t. P.
+         * @brief Calculate the Hessian of the scaling function F(P) = a^T P + b + 1 w.r.t. P.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return xt::xtensor<double, 2> d^2F/dPdP, shape: (dim_p, dim_p)
@@ -55,7 +55,7 @@ class Hyperplane3d : public ScalingFunction3d {
         xt::xtensor<double, 2> getBodyFdPdP(const xt::xtensor<double, 1>& P) const override;
 
         /**
-         * @brief Calculate the third order derivative of the scaling function F(P) = a^T P + b w.r.t. P.
+         * @brief Calculate the third order derivative of the scaling function F(P) = a^T P + b + 1 w.r.t. P.
          * 
          * @param P Position in the body frame, shape: (dim_p,)
          * @return xt::xtensor<double, 3> d^3F/dPdPdP, shape: (dim_p, dim_p, dim_p)
